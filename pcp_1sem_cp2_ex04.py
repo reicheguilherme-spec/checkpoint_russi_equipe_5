0ea0a526e53834c656fcc764defda0021501d9e9
nicolascnishi <nicolas.c.nishi@gmail.com>
2026-04-16 19:43:50 -0300

    Terminei o 4

--- pcp_1sem_cp2_ex04.py
+++ pcp_1sem_cp2_ex04.py
@@ -1,23 +1,56 @@
 nomeFuncionario = input("Digite seu nome: ")
 cargo = input("Digite seu cargo (1=Gerente, 2=Analista, 3=Assistente, 4=Estagiário): ")
-salarioBase = float(input("Digite o sálario: "))
-totalHorasExtrasTrab = input("Digite o total de horas extras trabalhadas: ")
-totalFaltas = input("Digite o total de faltas: ")
+salarioBase = float(input("Digite o salário: "))
+totalHorasExtrasTrab = int(input("Digite o total de horas extras trabalhadas: "))
+totalFaltas = int(input("Digite o total de faltas: "))
 bonus = input("Recebeu bônus (s/n): ")
 
-print(nomeFuncionario)
-print("Salário é R$" f"{salarioBase:.2f}")
-print("O total de horas trabalhadas é de" f"{totalHorasExtrasTrab}")
-print("O número total de faltas é" f"{totalFaltas}")
+
+def valorHorasExtrasTrab(salarioBase, totalHorasExtrasTrab):
+    return 0.015 * salarioBase * totalHorasExtrasTrab
+
+
+def descontoPorFalta(salarioBase, totalFaltas):
+    return 0.02 * salarioBase * totalFaltas
+
+
+def valorBonus(cargo, salarioBase):
+    if cargo == "1":
+        return salarioBase + 1000
+    elif cargo == "2":
+        return salarioBase + 500
+    elif cargo == "3":
+        return salarioBase + 300
+    elif cargo == "4":
+        return salarioBase + 100
+    else:
+        return salarioBase
+
+
+def salarioBruto(salarioBonus, valorExtra):
+    return salarioBonus + valorExtra
+
+
+def totalAcresimos(salarioBonus, salarioBase, valorExtra):
+    return (salarioBonus - salarioBase) + valorExtra
+
+
+def salarioFinal(salarioBruto, desconto):
+    return salarioBruto - desconto
+
+valor_extra = valorHorasExtrasTrab(salarioBase, totalHorasExtrasTrab)
+desconto = descontoPorFalta(salarioBase, totalFaltas)
+
 if bonus == "s":
-    print("Ele recebeu bônus")
+    salarioBonus = valorBonus(cargo, salarioBase)
 else:
-    print("Ele não recebeu bônus")
-if cargo == "1":
-    print("O cargo é Gerente")
-if cargo == "2":
-    print("O cargo é Analista")
-if cargo == "3":
-    print("O cargo é Assistente")
-if cargo == "4":
-    print("O cargo é Estagiário")
+    salarioBonus = salarioBase
+
+salario_bruto = salarioBruto(salarioBonus, valor_extra)
+acrescimos = totalAcresimos(salarioBonus, salarioBase, valor_extra)
+salario_final = salarioFinal(salario_bruto, desconto)
+
+print(f"O salário bruto é de R$ {salario_bruto:.2f}")
+print(f"O total de acréscimos é de R$ {acrescimos:.2f}")
+print(f"O total de desconto é de R$ {desconto:.2f}")
+print(f"O salário final é de R$ {salario_final:.2f}")
