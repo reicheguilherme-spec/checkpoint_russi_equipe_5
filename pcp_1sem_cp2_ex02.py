6b5086926ae25e49a8d43f6bbe776d176b112be4
lucas08dev <lucas.sabreu2008@gmail.com>
2026-04-16 22:42:04 -0300

    Exercício 2 - CP Russi: Finalizado

--- pcp_1sem_cp2_ex02.py
+++ pcp_1sem_cp2_ex02.py
@@ -16,17 +16,18 @@ print(f"A = {a}, B = {b}, C = {c}")
 if a >= b + c:
     print("NAO FORMA TRIANGULO")
 
-elif a == b and b == c:
-    print("TRIANGULO EQUILATERO")
-
-elif a == b or b == c or c == a:
-    print("TRIANGULO ISOSCELES")
-
-elif a**2 == b**2 + c**2:
-    print("TRIANGULO RETANGULO")
-
-elif a**2 > b**2 + c**2:
-    print("TRIANGULO OBTUSANGULO")
-
 else:
-    print("TRIANGULO ACUTANGULO")
+    if a**2 == b**2 + c**2:
+        print("TRIANGULO RETANGULO")
+
+    elif a**2 > b**2 + c**2:
+        print("TRIANGULO OBTUSANGULO")
+
+    else:
+        print("TRIANGULO ACUTANGULO")
+
+    if a == b and b == c:
+        print("TRIANGULO EQUILATERO")
+
+    elif a == b or b == c or c == a:
+        print("TRIANGULO ISOSCELES")
